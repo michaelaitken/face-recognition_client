0c3d98874483a404ce51911497156bcbf8599680
Michael Aitken <mike@mikeaitken.net>
2023-02-19 16:25:58 +0800

    Added Heroku Backend Link

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://secret-refuge-95501.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -80,7 +80,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://secret-refuge-95501.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
