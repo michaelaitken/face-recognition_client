d2c76c1e793222ea258305cb2906469360366065
Michael Aitken <mike@mikeaitken.net>
2023-02-19 18:18:10 +0800

    Updated to gRPC API

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
   // A helper function that sets the `user` object in the state.
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const clarifaiFace = data.data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
